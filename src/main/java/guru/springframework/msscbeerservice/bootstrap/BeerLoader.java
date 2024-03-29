e1a3e5f1c1b85ed1b8be2cb37d1032850f6af1a5
Trung Ho <hobao.trung12345@gmail.com>
2024-03-30 04:49:50 +0700

    add beer loading bootstrap

---
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,50 @@
+package guru.springframework.msscbeerservice.bootstrap;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.repository.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        System.out.println("Initialize beer objects...");
+
+        if (beerRepository.count() == 0) {
+            System.out.println("Beer database is empty. Creating beer samples...");
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+
+        System.out.println("Number of loaded beers: " + beerRepository.count());
+    }
+}
